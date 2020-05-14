92f243a3e820d951069a81e5fafb58f7f26177d1
Bessim Boujebli <bessim.boujebli@gmail.com>
2020-05-14 16:49:21 +0100

    Updates

---
+++ precache-manifest.a1d1d96fb5573f140cf6775ede9584f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07887854208cd98f02f26b59dc007cb9",
+    "url": "/robomon/index.html"
+  },
+  {
+    "revision": "717fcb5e0a00517cfb03",
+    "url": "/robomon/static/css/main.2e75fb2a.chunk.css"
+  },
+  {
+    "revision": "d090dd4beb2e925a1036",
+    "url": "/robomon/static/js/2.b76b8dff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robomon/static/js/2.b76b8dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717fcb5e0a00517cfb03",
+    "url": "/robomon/static/js/main.06bad0a9.chunk.js"
+  },
+  {
+    "revision": "432ae656ce6b7276dce0",
+    "url": "/robomon/static/js/runtime-main.a4060f81.js"
+  },
+  {
+    "revision": "3ee46ced80f90d1d072bed2ac13acf7b",
+    "url": "/robomon/static/media/COMPUTER-Robot.ttf.3ee46ced.woff"
+  },
+  {
+    "revision": "56f2cddee2788b0cf47c8f61dfe86731",
+    "url": "/robomon/static/media/COMPUTER-Robot.ttf.56f2cdde.svg"
+  },
+  {
+    "revision": "ed1a0f7d8e3a7d4a5cf0e7f80fc3e4b0",
+    "url": "/robomon/static/media/COMPUTER-Robot.ttf.ed1a0f7d.eot"
+  }
+]);
